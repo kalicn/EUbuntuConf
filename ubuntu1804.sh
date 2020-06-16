e78938ccc9e52ab2fe7c27bdd5943982bcb6d9f7
Kali <64886201+kalicn@users.noreply.github.com>
2020-06-16 14:45:45 +0800

    Create ubuntu1804.sh

---
+++ ubuntu1804.sh
@@ -0,0 +1,24 @@
+# /bin/bash
+
+printf 请使用Sudo运行
+
+printf 喜欢给个Star吧~
+
+sudo apt-get update
+sudo apt-get install gcc g++ gdebi gnome-tweak-tool
+sudo apt-get install chrome-gnome-shell git
+sudo snap install communitheme
+
+printf 即将开始更换Shell，请保持网络通畅及程序运行
+
+sudo apt-get install zsh
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+./install.sh
+chsh -s /bin/zsh
+
+printf 下一步更换主题请自行前往https://kalicn.github.io/2020/05/30/搭建Ubuntu工作环境
+sudo apt-get install gnome-shell-extentions
+
+printf 安装结束，给个star吧~
+
+
